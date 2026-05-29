485cfbb1306164c15e3fd135f4aec7956d85db94
千寻啊千寻 <960339491@qq.com>
2026-05-29 15:34:34 +0800

    Initial Commit

---
+++ MouseDance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MouseDance
+//
+//  Created by 千寻啊千寻 on 2026/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MouseDance/MouseDanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseDanceApp.swift
+//  MouseDance
+//
+//  Created by 千寻啊千寻 on 2026/5/29.
+//
+
+import SwiftUI
+
+@main
+struct MouseDanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
